aa7ae456d1b14b81fa921edff7f6a05aa995e83f
Martino <60008678+Martin-Tule@users.noreply.github.com>
2024-10-15 10:50:43 +0200

    added update & alter statements

---
+++ COMMITS_UPDATE&ALTER_TYPE.sql
@@ -0,0 +1,26 @@
+
+--region update rum to have new field status and migrate data from old fields to there. 
+ALTER TABLE rum
+ADD COLUMN status ENUM('checkat_in', 'checkat_ut', 'städas', 'underhållsarbetas');
+
+UPDATE rum
+SET status = CASE
+    WHEN checked_in = TRUE THEN 'checkat_in'
+    WHEN checked_out = TRUE THEN 'checkat_ut'
+    -- Add any other conditions if needed
+    ELSE status  -- To handle cases where neither is set
+END;
+
+ALTER TABLE rum
+DROP COLUMN checked_in,
+DROP COLUMN checked_out;
+-- endregion
+
+-- region 
+-->>rum
+--istället för att ha checked_in och checked_out ha enbart antingen checked_in eller eventuellt "status".
+--Fördel med status är att kunna ha en lista över händelser som påverkar rummet, exempelvis: 
+ALTER TABLE faktura
+ADD COLUMN status ENUM('checkat_in', 'checkat_ut', 'städas', 'underhållsarbetas')
+--TODO:
+-- note do not have testdate created for this yet! 
