34037157c8b7327a5bdcd90b643b63681bc29ea0
Martino <60008678+Martin-Tule@users.noreply.github.com>
2024-10-15 11:44:34 +0200

    made query for Se vem i personalen som gjort bokning

--- COMMITS_SELECT_TYPE.sql
+++ COMMITS_SELECT_TYPE.sql
@@ -4,3 +4,11 @@
 */
 SELECT datum, antal_personer, grupp_bokning_id FROM middag 
 ORDER BY datum;
+
+/*Se vem i personalen som gjort bokning 
+        Selectsats med sökning i bokning samt personal
+        *** SQL QUERY:::
+        */
+SELECT personal.fornamn, personal.efternamn, bokning.bokning_id, personal.personal_id
+FROM personal
+INNER JOIN bokning ON personal.personal_id = bokning.personal_id;
