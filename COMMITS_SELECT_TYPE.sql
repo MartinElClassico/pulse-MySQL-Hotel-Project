4bd0dd014038248030c29e903d0c6b5c2bd542fc
Eynaan <annaemgr@gmail.com>
2024-10-15 11:54:46 +0200

    More example queries

--- COMMITS_SELECT_TYPE.sql
+++ COMMITS_SELECT_TYPE.sql
@@ -6,9 +6,16 @@ SELECT datum, antal_personer, grupp_bokning_id FROM middag
 ORDER BY datum;
 
 /*Se vem i personalen som gjort bokning 
-        Selectsats med sökning i bokning samt personal
-        *** SQL QUERY:::
-        */
+Selectsats med sökning i bokning samt personal
+*** SQL QUERY:::
+*/
 SELECT personal.fornamn, personal.efternamn, bokning.bokning_id, personal.personal_id
 FROM personal
 INNER JOIN bokning ON personal.personal_id = bokning.personal_id;
+
+/* Se vilka rum som är utcheckade
+Relativt enkel select-sats
+*** SQL QUERY:::
+*/
+SELECT rum_id FROM rum 
+WHERE status = 'checkat_ut';
