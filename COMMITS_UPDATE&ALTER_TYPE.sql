6b3dbb29cf42baaec3e7b3316dd0f43f62ef608c
Martino <60008678+Martin-Tule@users.noreply.github.com>
2024-10-15 10:52:34 +0200

    fixed comment syntax

--- COMMITS_UPDATE&ALTER_TYPE.sql
+++ COMMITS_UPDATE&ALTER_TYPE.sql
@@ -1,5 +1,5 @@
 
---region update rum to have new field status and migrate data from old fields to there. 
+-- region update rum to have new field status and migrate data from old fields to there. 
 ALTER TABLE rum
 ADD COLUMN status ENUM('checkat_in', 'checkat_ut', 'städas', 'underhållsarbetas');
 
@@ -10,7 +10,7 @@ SET status = CASE
     -- Add any other conditions if needed
     ELSE status  -- To handle cases where neither is set
 END;
---endregion
+-- endregion
 
 ALTER TABLE rum
 DROP COLUMN checked_in,
@@ -18,11 +18,11 @@ DROP COLUMN checked_out;
 -- endregion
 
 -- region 
--->>rum
---istället för att ha checked_in och checked_out ha enbart antingen checked_in eller eventuellt "status".
---Fördel med status är att kunna ha en lista över händelser som påverkar rummet, exempelvis: 
+-- >>rum
+-- istället för att ha checked_in och checked_out ha enbart antingen checked_in eller eventuellt "status".
+-- Fördel med status är att kunna ha en lista över händelser som påverkar rummet, exempelvis: 
 ALTER TABLE faktura
 ADD COLUMN status ENUM('checkat_in', 'checkat_ut', 'städas', 'underhållsarbetas')
---TODO:
+-- TODO:
 -- note do not have testdate created for this yet! 
---endregion
+-- endregion
